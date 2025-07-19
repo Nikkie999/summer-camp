6b0eea355bbf712c923164e1e98646d135a076b0
Nikkie999 <nkie339@gmail.com>
2025-07-19 22:31:30 +0300

    Initial commit with day1, day2, day3 C# projects and .gitignore

---
+++ RiderProjects/day.2/day.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string correctLogin = "Nikkie";
+        string correctPassword = "12345";
+
+        Console.Write("Введіть логін: ");
+        string login = Console.ReadLine();
+
+        if (login != correctLogin)
+        {
+            Console.WriteLine("Невірний логін");
+        }
+        else
+        {
+            Console.Write("Введіть пароль: ");
+            string password = Console.ReadLine();
+
+            if (password != correctPassword)
+            {
+                Console.WriteLine("Невірний пароль");
+            }
+            else
+            {
+                Console.WriteLine("Вхід дозволено");
+            }
+        }
+    }
+}
