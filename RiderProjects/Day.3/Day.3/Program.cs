2b15cb0c4a3eba3731d0f2812617dee4209758f9
Nikkie999 <nkie339@gmail.com>
2025-07-21 22:57:32 +0300

    Вирішені конфлікти

---
+++ RiderProjects/Day.3/Day.3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine($"i = {i}");
+        }
+        
+        int j = 1;
+        while (j <= 5)
+        {
+            Console.WriteLine($"j = {j}");
+            j++;
+            
+        }
+        
+        int k = 1;
+        do
+        {
+            Console.WriteLine($"k = {k}");
+            k++;
+           //break;
+            //continue;
+            
+        } while (k <= 5);
+
+        int a = 0;
+        Label:
+        a++;
+        Console.WriteLine($"A = {a}");
+        if (a < 5)
+        {
+            goto Label;
+        }
+        Console.ReadKey();
+    }
+}
