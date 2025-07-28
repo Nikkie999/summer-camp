6e378b56eef2f5ee36b4e8df780db6db0c90a499
Nikkie999 <nkie339@gmail.com>
2025-07-28 11:39:42 +0300

    Додано проект Day6

---
+++ RiderProjects/Day6/Day6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        PrintNumbers(1, 5);
+        Print(5);
+    }
+
+    static void PrintNumbers(int digital, int max)
+    {
+        if (digital > max)
+        {
+            return;
+        }
+
+        Console.WriteLine(digital);
+        PrintNumbers(digital + 1, max);
+    }
+
+    static void Print(int number)
+    {
+        if (number < 1)
+        {
+            return;
+        }
+
+        Console.WriteLine(number);
+        Print(number - 1);
+    }
+}
