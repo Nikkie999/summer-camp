6c6f0889d2f6da0168923a0a86a6a1625f28673c
Nikkie999 <nkie339@gmail.com>
2025-07-30 15:14:24 +0300

    Update Program.cs

--- Day4/Day4/Program.cs
+++ Day4/Day4/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть а: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введіть b: ");
+        int b = int.Parse(Console.ReadLine());
+
+        if (a > 0 && b > 0)
+        {
+            Console.WriteLine("Так ці числа більше нуля");
+        }
+
+        if (a < 0 || b < 0)
+        {
+            Console.WriteLine("Щось одне менше нуля");
+        }
+
+        if (!(a != b))
+        {
+            Console.WriteLine("І ще");
+            Console.WriteLine("А дорівнює B");
+        }
+    }
+}
