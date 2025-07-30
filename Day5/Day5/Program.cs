de890cf7fb91e395e1e1f6941a9229e10b7a097c
Nikkie999 <nkie339@gmail.com>
2025-07-30 15:14:47 +0300

    Update Program.cs

--- Day5/Day5/Program.cs
+++ Day5/Day5/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       Say();
+       Console.WriteLine(Method(10));
+       Console.WriteLine(Methoddd(5, 3));
+    }
+    static void Say()
+    {
+        Console.WriteLine("Привіт");
+    }
+
+    static int Method(int x)
+    {
+        return 5 + x;
+    }
+
+    static int Methoddd(int a, int b)
+    {
+        return a + b;
+    }
+
+   
+}
