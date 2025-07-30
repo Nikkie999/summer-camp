0c1814fea0d96bcee6b4c5113c273328d09ee4ce
Nikkie999 <nkie339@gmail.com>
2025-07-30 15:15:09 +0300

    Update Program.cs

--- Day6/Day6/Program.cs
+++ Day6/Day6/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        PrintNumbers(1, 5);
+        Print(5);
+    }
+
+    static void PrintNumbers(int digital, int max)
+    {
+        if (digital > max)
+        {
+            return;
+        }
+
+        Console.WriteLine(digital);
+        PrintNumbers(digital + 1, max);
+    }
+
+    static void Print(int number)
+    {
+        if (number < 1)
+        {
+            return;
+        }
+
+        Console.WriteLine(number);
+        Print(number - 1);
+    }
+}
